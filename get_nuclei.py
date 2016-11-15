2d8951cc96def097d80c09b9c2eef98e92b74d78
Porfirio Quintero <porfirioqc@gmail.com>
2016-11-14 19:13:44 -0800

    function to segment nuclei, peaks did not work

--- get_nuclei.py
+++ get_nuclei.py
@@ -13,6 +13,7 @@ from scipy import ndimage
 from skimage.draw import circle_perimeter
 from scipy.spatial.distance import pdist, squareform
 import matplotlib.gridspec as gridspec
+from scipy.spatial import cKDTree
 #from skimage.filters import try_all_threshold
 
 %matplotlib
@@ -62,8 +63,8 @@ def im_hist(im):
 
 def rectangleROI(im, thresh=None):
     """
-    Identify coordinates of rectangular ROI
-    which does not contain all continuous zero columns and rows after threshold
+    Identify coordinates of rectangular ROI in an image with mostly dark irrelevant pixels
+    ROI does not contain all continuous zero columns and rows after threshold
     """
     if not thresh:
         thresh = skimage.filters.threshold_li(im)
@@ -89,9 +90,9 @@ def label_sizesel(im, im_mask, bounds=(100, 1500)):
     nuclei = skimage.measure.regionprops(markers, im)
     areas = [n.area for n in nuclei]
     # labels of markers within area bounds
-    labels_ = [n.label for n in nuclei_rfp if bounds[0] < n.area < bounds[1]]
+    labels_ = [n.label for n in nuclei if bounds[0] < n.area < bounds[1]]
     # remove markers outside bounds
-    ix = np.in1d(markers.ravel(), _labels).reshape(markers.shape)
+    ix = np.in1d(markers.ravel(), labels_).reshape(markers.shape)
     markers[~ix] = 0
     # update nuclei regionprops list
     nuclei = [n for n in nuclei if n.label in labels_]
@@ -103,30 +104,20 @@ def nuclei_int(im_r, im_g, plot=False, min_distance=10):
     """
 
     thresh_r = skimage.filters.threshold_li(im_r)
+    thresh_g = skimage.filters.threshold_li(im_g)
     # get identical ROI in of both images
     roi = rectangleROI(im_r, thresh_r)
     im_r = im_r[roi]
     im_g = im_g[roi]
 
-    # get nuclei markers 
-    nuclei = peak_local_max(im_r, indices=True, threshold_abs=thresh_r, 
-            min_distance=min_distance)
-
-    # remove oversaturated nuclei
-    nuclei = [tuple(n) for n in nuclei if im_r[tuple(n)] < 4000]
-
-    # remove markers that are too close to each other
-    dist = squareform(pdist(nuclei))
-    ind = np.where((dist<25)&(dist>0))[0]
-    too_close = [ix for (i, ix) in enumerate(ind) if i%2]
-    for i in too_close: nuclei.pop(i)
+    nuclei_r = get_nuclei_peaks(im_r, thresh_r, min_distance)
+    nuclei_g = get_nuclei_peaks(im_g, thresh_g, min_distance)
 
     # get intensities of non-oversaturated nuclei
     int_r = np.array([im_r[n] for n in nuclei])
     int_g = np.array([im_g[n] for n in nuclei])
     # only use intensities if both images are above thresh_g
     # as we already know that int_r are all above thresh_r
-    thresh_g = skimage.filters.threshold_li(im_g)
     i_thresh = np.where(int_g > thresh_g)
     int_r, int_g = int_r[i_thresh], int_g[i_thresh]
 
@@ -139,6 +130,37 @@ def nuclei_int(im_r, im_g, plot=False, min_distance=10):
 
     return im_plot, int_ratio, int_r, int_g
 
+def get_nuclei_peaks(im, thresh, min_distance):
+    # get nuclei markers 
+    nuclei = peak_local_max(im, indices=True, threshold_abs=thresh, 
+            min_distance=min_distance)
+
+    # remove oversaturated nuclei
+    nuclei = [tuple(n) for n in nuclei if im[tuple(n)] < 4000]
+
+    # remove markers that are too close to each other because of flat areas
+    nuclei_ = []
+    for i in range(len(nuclei)-1):
+        if not np.isclose(im[nuclei[i]], im[nuclei[i+1]]):
+            nuclei_.append(nuclei[i])
+
+#    # remove markers that are too close to each other
+#    dist = squareform(pdist(nuclei))
+#    ind = np.where((dist<min_distance)&(dist>0))
+#    too_close = [ix for (i, ix) in enumerate(ind) if i%2][0]
+#    #new_nuclei = [nuclei[i] for i in ]
+#    if len(too_close) > 0: 
+#        for i in too_close: nuclei.pop(i)
+    return nuclei_
+
+def pair_nuclei(nuclei_r, nuclei_g):
+    """
+    Pair closest nuclei UNFINISHED
+    """
+    KDtree = cKDTree(nuclei_r)
+    nuclei_g_ = [n for n in nuclei_g if KDtree.query(n)[0] < 30]
+    return nuclei_g_
+
 def circle_nuclei(nuclei, im):
     """
     Draw circles around identified nuclei for plotting
@@ -150,17 +172,29 @@ def circle_nuclei(nuclei, im):
         im_plot[circle] = np.max(im_plot) 
     return im_plot
 
-def plot_nuceli_int(im_plot, int_ratio):
-    gs = gridspec.GridSpec(1, 4)
+def plot_nuceli_int(im_plot_r, im_plot_g, int_ratio):
+    gs = gridspec.GridSpec(1, 6)
     ax1 = plt.subplot(gs[0:3])
-    ax2 = plt.subplot(gs[3])
-    ax1.imshow(im_plot, plt.cm.viridis)
-    sns.stripplot(int_ratio, orient='v', size=10, alpha=0.5, cmap='viridis', ax=ax2)
+    ax2 = plt.subplot(gs[3:6])
+    ax3 = plt.subplot(gs[6])
+    ax1.imshow(im_plot_r, plt.cm.viridis)
+    ax2.imshow(im_plot_g, plt.cm.viridis)
+    sns.stripplot(int_ratio, orient='v', size=10, alpha=0.5, cmap='viridis', ax=ax3)
     #ax2.tick_params(axis='y', which='both', labelleft='off', labelright='on')
     ax2.set_ylabel('intensity ratio', fontsize=20)
     #ax2.yaxis.set_label_position("right")
     plt.tight_layout()
     return None
+
+def mask_image(im):
+    """
+    Create a binary mask to segment nuclei
+    """
+    im_thresh = threshold_adaptive(im, 15)
+    im_thresh = skimage.morphology.remove_small_objects(im_thresh, min_size=200)
+    im_thresh = ndimage.morphology.binary_fill_holes(im_thresh, morphology.disk(1.8))
+    im_thresh = morphology.binary_opening(im_thresh, morphology.disk(3))
+    return im_thresh
 
 gfp, rfp = gfp[1:], rfp[1:]
 for im_num in range(len(rfp)):
@@ -171,11 +205,7 @@ for im_num in range(len(rfp)):
     #im = skimage.exposure.equalize_adapthist(im)
     # get roi from rfp and apply to both
 
-    #im_thresh = threshold_adaptive(im, 15)
-    #im_thresh = morphology.binary_opening(im_thresh, morphology.disk(3))
-    #im_thresh = skimage.morphology.remove_small_objects(im_thresh, min_size=200)
 
-    #im_thresh = ndimage.morphology.binary_fill_holes(im_thresh, morphology.disk(1.8))
     #im_thresh = ndimage.morphology.binary_closing(im_thresh)
     #im_thresh = morphology.binary_opening(im_thresh, morphology.disk(5))
 
@@ -199,3 +229,6 @@ for im_num in range(len(rfp)):
     #intensities = [n.max_intensity for n in nuclei]
     im_plot, int_ratio, i1, i2 = nuclei_int(im_r, im_g, plot=True)
 
+io.imshow(im_r)
+markers = plt.ginput(10)
+morphology.reconstruction(
